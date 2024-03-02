b54c43c300b5591cf56ce783700b5552e6b1f60a
franmuline <franmb2000@gmail.com>
2024-03-02 23:20:16 +0100

    Small changes main.py

--- Week 2/main.py
+++ Week 2/main.py
@@ -8,10 +8,8 @@ from train import random_search
 def main():
     # Read arguments
     parser = argparse.ArgumentParser(description="C5 - Week 2")
-    parser.add_argument("--action", type=str, default="simple_inference", help="Action to perform, can be 'simple_inference', 'eval_inference', 'fine_tune' or 'random_search'")
-    parser.add_argument("--model", type=str, default="mask_rcnn", help="Model to use, can be 'mask_rcnn' or 'faster_rcnn'")
-    parser.add_argument("--config", type=str, default=None, help="For 'fine_tune' or 'random_search' actions, path to the configuration file to use")
-    parser.add_argument("--wandb", type=bool, default=False, help="Enable Weights & Biases logging")
+    parser.add_argument("--action", type=str, default="random_search", help="Action to perform, can be 'simple_inference', 'eval_inference', 'fine_tune' or 'random_search'")
+    parser.add_argument("--model", type=str, default="faster_rcnn", help="Model to use, can be 'mask_rcnn' or 'faster_rcnn'")
 
     args = parser.parse_args()
 
