779d5e904963e338db3aca713823332fd67dc5aa
franmuline <franmb2000@gmail.com>
2024-03-14 12:19:07 +0100

    Added code from https://github.com/adambielski/siamese-triplet/

--- Week 3/models.py
+++ Week 3/models.py
@@ -11,3 +11,33 @@ class ResNet50(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+# CODE EXTRACTED FROM: https://github.com/adambielski/siamese-triplet/blob/master/networks.py
+class SiameseNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(SiameseNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        return output1, output2
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
+
+
+class TripletNet(nn.Module):
+    def __init__(self, embedding_net):
+        super(TripletNet, self).__init__()
+        self.embedding_net = embedding_net
+
+    def forward(self, x1, x2, x3):
+        output1 = self.embedding_net(x1)
+        output2 = self.embedding_net(x2)
+        output3 = self.embedding_net(x3)
+        return output1, output2, output3
+
+    def get_embedding(self, x):
+        return self.embedding_net(x)
